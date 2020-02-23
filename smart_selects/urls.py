5812a54e55126c9efc37859260b50b7bb14bd0bd
xncandela <ncandela@theexcellencecollection.com>
2020-02-23 09:53:52 -0500

    Modificacion de url.py para version 3

--- smart_selects/urls.py
+++ smart_selects/urls.py
@@ -1,14 +1,9 @@
-from smart_selects import views
-try:
-    from django.conf.urls.defaults import url
-except ImportError:
-    from django.conf.urls import url
+from smart_selects.views import filterchain_all,filterchain
+from django.urls import path
 
 urlpatterns = [
-    url(r'^all/(?P<app>[\w\-]+)/(?P<model>[\w\-]+)/(?P<field>[\w\-]+)/(?P<foreign_key_app_name>[\w\-]+)/(?P<foreign_key_model_name>[\w\-]+)/(?P<foreign_key_field_name>[\w\-]+)/(?P<value>[\w\-,]+)/$',  # noqa: E501
-        views.filterchain_all, name='chained_filter_all'),
-    url(r'^filter/(?P<app>[\w\-]+)/(?P<model>[\w\-]+)/(?P<field>[\w\-]+)/(?P<foreign_key_app_name>[\w\-]+)/(?P<foreign_key_model_name>[\w\-]+)/(?P<foreign_key_field_name>[\w\-]+)/(?P<value>[\w\-,]+)/$',  # noqa: E501
-        views.filterchain, name='chained_filter'),
-    url(r'^filter/(?P<app>[\w\-]+)/(?P<model>[\w\-]+)/(?P<manager>[\w\-]+)/(?P<field>[\w\-]+)/(?P<foreign_key_app_name>[\w\-]+)/(?P<foreign_key_model_name>[\w\-]+)/(?P<foreign_key_field_name>[\w\-]+)/(?P<value>[\w\-,]+)/$',  # noqa: E501
-        views.filterchain, name='chained_filter'),
+    path('all/<app>/<model>/<field>/<foreign_key_app_name>/<foreign_key_model_name>/<foreign_key_field_name>/<value>/',filterchain_all, name='chained_filter_all'),
+    path('filter/<app>/<model>/<field>/<foreign_key_app_name>/<foreign_key_model_name>/<foreign_key_field_name>/<value>/',filterchain, name='chained_filter'),
+    path('filter/<app>/<model>/<manager>/<field>/<foreign_key_app_name>/<foreign_key_model_name>/<foreign_key_field_name>/<value>/',filterchain, name='chained_filter'),
 ]
+
